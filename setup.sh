3b2265040d44ca117028dc402d9b49b9af7c5534
deadspace92 <44837910+deadspace92@users.noreply.github.com>
2023-02-27 16:09:02 -0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+apt update
+apt upgrade
+apt install nginx -y
+apt install dnsmasq -y
+apt install python -y
+apt install neofetch -y
+apt install wget -y
+apt install git -y
+wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm.tgz
+tar xvzf ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
+ngrok authtoken 2MLMTFRyecmrXiqZF3AfExmLXoa_7r71gweNku4LcEEk8EdFy
+nginx
