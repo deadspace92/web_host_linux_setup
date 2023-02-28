e271a62ac67a63181cfe2421bb5cb28eb3044309
deadspace92 <44837910+deadspace92@users.noreply.github.com>
2023-02-27 16:27:49 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,6 @@ apt install neofetch -y
 apt install wget -y
 apt install git -y
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm.tgz
-tar xvzf ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
+tar xvzf ngrok-v3-stable-linux-amd64.tgz
 ngrok authtoken 2MLMTFRyecmrXiqZF3AfExmLXoa_7r71gweNku4LcEEk8EdFy
 nginx
